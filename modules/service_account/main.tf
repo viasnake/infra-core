96531d5c20f0d7a213b24ae3333ec54448d9cc99
snake <admin@viasnake.com>
2025-04-27 18:14:39 +0900

    enable iam.googleapis.com

--- modules/service_account/main.tf
+++ modules/service_account/main.tf
@@ -1,7 +1,15 @@
+resource "google_project_service" "main" {
+  project            = var.project
+  service            = "iam.googleapis.com"
+  disable_on_destroy = false
+}
+
 resource "google_service_account" "main" {
   account_id   = var.account_id
   display_name = var.display_name
   description  = var.description
   disabled     = var.disabled
   project      = var.project
+
+  depends_on = [google_project_service.main]
 }
